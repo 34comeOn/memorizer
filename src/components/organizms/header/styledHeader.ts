230d8600088e2345e08997d1da7f6b2836d2b481
Mikhail <barabanovm87@gmail.com>
2023-05-18 16:37:16 +0300

    changes style concept

--- src/components/organizms/header/styledHeader.ts
+++ src/components/organizms/header/styledHeader.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import { BACKGROUND_COLOR } from "../../../constants/styleConstants";
 
 export const StyledHeader = styled.header`
     height: 115px;
-    background-color: black;
+    background-color: ${BACKGROUND_COLOR};
     padding: 0 20px;
     position: relative;
-    box-shadow: 0px 14px 19px -12px rgb(189 167 174);
+    box-shadow: 0px 29px 24px -33px rgb(189 167 174);
     
     display: flex;
     align-items: center;
