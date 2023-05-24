44a57bed14448c31ac20e3a2656e60e5dedd4c4f
Mikhail <barabanovm87@gmail.com>
2023-05-24 12:27:08 +0300

    adds form to new collection page

--- src/pages/newCollectionPage/index.tsx
+++ src/pages/newCollectionPage/index.tsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { Form, Formik } from "formik";
+import { FormInput } from "../../components/molecules/formInput";
+import './style.scss';
 
 export const NewCollectionPage = () => {
     return(
         <>
-            <p style={{color: 'white'}}>
-                Create a new collection
-            </p>
+            <Formik 
+                initialValues={{ 
+                    title: '', 
+                    isPrivate: true, 
+                }}
+                onSubmit={
+                ()=> console.log('hi')
+                }
+            >
+                {()=>{
+                    return(
+                        <Form className='new-collection--form'>
+                            <FormInput 
+                                type='text' 
+                                name='title' 
+                                placeholder='' 
+                                labelValue='Come up with title of your new collection'
+                            />
+                            <button className='submit--button' type='submit'>
+                                Create collection
+                            </ button>
+                        </Form>
+                    )
+                }}
+            </ Formik>
         </>
     )
 }
